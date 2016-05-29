6372849609fa0e798c31e258b445626ad5c94aa5
duncanunderwood <dunca@WIN02-MBP>
2016-05-30 02:43:24 +1000

    questions

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,6 +10,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            /*
             int x;
             int y;
 
@@ -17,6 +18,41 @@ namespace HelloWorld
             y = x + 3;
             Console.WriteLine(y);
             Console.ReadLine();
+            */
+
+            // Questions 
+            Console.WriteLine("Enter the following details");
+            Console.Write("Type your first name: ");
+            string myFirstName = Console.ReadLine();
+
+            Console.Write("Type your last name: ");
+            string myLastName = Console.ReadLine();
+
+            Console.Write("What is your partner's name: ");
+            string partnerName = Console.ReadLine();
+
+            Console.Write("What is your age: ");
+            string myAge = Console.ReadLine();
+
+            Console.Write("What is your partners age: ");
+            string partnerAge = Console.ReadLine();
+
+            // Age Calculate
+            int d;
+            int l;
+            int z;
+
+            // String convert to an Int
+            d = int.Parse(partnerAge);
+            d = Convert.ToInt32(partnerAge);
+            l = int.Parse(myAge);
+            l = Convert.ToInt32(myAge);
+            z = d - l;
+
+            // Final output code
+            Console.WriteLine("Hello " + myFirstName + " " + myLastName + ", you are " + z + " years younger then " + partnerName + " and he loves you, Congratulations " +  ":)" );
+            Console.ReadLine();
+
         }
     }
 }
